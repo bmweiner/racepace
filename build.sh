0d95cc6c468a1afe9738d0f450521e33d1438a7c
bmweiner <bmweiner@users.noreply.github.com>
2017-08-27 13:31:13 -0400

    Fixed path issue.

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
+BASEDIR=$(dirname $0)
+cd $BASEDIR
 pip install -U -r requirements.txt -t dist/lambda
 cp -R racepace/* dist/lambda
 cd dist/lambda
-zip -r ../lambda.zip ./*
+zip -r ../lambda.zip .  # zip within dir to avoid parent folder
 cd ..
 rm -r lambda
